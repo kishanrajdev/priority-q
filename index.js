3ea83d6e1d6af03e700120603182924e8af18390
kishanrajdev <kishanrajdev047@gmail.com>
2021-08-09 20:43:44 +0530

    adding index and tests

---
+++ index.js
@@ -0,0 +1,98 @@
+class Heap {
+  constructor (collection = [], comparator) {
+    this.comparator = comparator;
+    if (comparator === undefined) {
+      this.comparator = (a, b) => {
+        if (a < b) return -1
+        else if (a > b) return 1;
+        return 0;
+      };
+    }
+    this.heap = collection;
+    this.size = collection.length;
+    this.createHeap();
+  }
+
+  static left(i) {
+    return 2 * i + 1;
+  }
+
+  static right(i) {
+    return 2 * i  + 2;
+  }
+
+  static parent(i) {
+    return Math.floor((i - 1) / 2);
+  }
+
+  swap(i, j) {
+    let temp = this.heap[i];
+    this.heap[i] = this.heap[j];
+    this.heap[j] = temp;
+  }
+
+  createHeap() {
+    const nonLeaf = Math.floor(this.size / 2) - 1;
+
+    for (let i = nonLeaf; i >= 0; i--) {
+      this.bubbleDown(i);
+    }
+  }
+
+  bubbleDown(i) {
+    if (i > Math.floor(this.size / 2) - 1) return;
+    let currEle = this.heap[i];
+    let left = Heap.left(i);
+    let right = Heap.right(i);
+    
+    let newIndex = i;
+    if (left < this.size && this.comparator(currEle, this.heap[left]) > 0) {
+      currEle = this.heap[left];
+      newIndex = Heap.left(i);
+    }
+    if (right < this.size && this.comparator(currEle, this.heap[right]) > 0) {
+      currEle = this.heap[right];
+      newIndex = Heap.right(i);
+    }
+    if (newIndex !== i) {
+      this.swap(i, newIndex)
+      this.bubbleDown(newIndex);
+    }
+  }
+
+  bubbleUp(i) {
+    while (i > 0) {
+      let parent = this.heap[Heap.parent(i)];
+      if (this.comparator(parent, this.heap[i]) > 0) {
+        this.swap(i, Heap.parent(i));
+      }
+      i = Heap.parent(i);
+    }
+  }
+
+  peek() {
+    if (this.size <= 0) {
+      throw new Error('no elements in heap.')
+    }
+    return this.heap[0];
+  }
+
+  poll() {
+    if (this.size === 0) {
+      throw new Error('no elements in heap.')
+    }
+    this.swap(0, this.size - 1);
+    const top = this.heap.pop();
+    this.size--;
+    this.bubbleDown(0);
+    return top;
+  }
+
+  add(ele) {
+    this.heap.push(ele);
+    this.size++;
+    this.bubbleUp(this.size - 1);
+  }
+}
+
+module.exports = Heap;
---
+++ test/test.js
@@ -0,0 +1,79 @@
+const assert = require('assert').strict;
+var PriorityQ = require('..')
+
+describe('PriorityQ', function() {
+
+  it('returns a new priority queue', () => {
+    assert(new PriorityQ() instanceof PriorityQ);
+  });
+
+  it('accepts a custom comparator function', () => {
+    const pq = new PriorityQ([1, 0.8, 9, 15, 0.5], (a, b) => b - a);
+    assert(pq instanceof PriorityQ && pq.peek() == 15);
+  });
+  
+});
+
+describe('Default comparator', () => {
+  it('returns min priority queue with numbers', () => {
+    const pq = new PriorityQ([10 , 5, 3, 11]);
+    assert(pq.peek() === 3);
+  });
+
+  it('returns min priority queue with strings', () => {
+    const pq = new PriorityQ(['Glow', 'Glee', 'a', 'Gland']);
+    assert(pq.peek() == 'Gland');
+  });
+});
+
+describe('Poll', () => {
+  const pq = new PriorityQ(['Glow', 'Glee', 'a', 'Gland']);
+
+  it('removes and returns the first element in the queue', () => {
+    const pq = new PriorityQ(['Glow', 'Glee', 'a', 'Gland']);
+    assert(pq.poll() == 'Gland' && pq.size === 3 && pq.peek() === 'Glee');
+  });
+
+  it('throws error when no elements in the queue', () => {
+    const pq = new PriorityQ();
+    assert.throws(pq.poll, Error);
+  });
+});
+
+describe('Peek', () => {
+  it('returns the first element of the priority queue', () => {
+    const pq = new PriorityQ(['Glow', 'Glee', 'a', 'Gland']);
+    assert(pq.peek() === 'Gland');
+  });
+
+  it('throws error when no elements in the queue', () => {
+    const pq = new PriorityQ();
+    assert.throws(pq.peek, Error);
+  });
+});
+
+describe('add', () => {
+  const minPq = new PriorityQ();
+  const maxPq = new PriorityQ([], (a, b) => b - a);
+  it('adds element in the min priority queue', () => {
+    minPq.add(10);
+    assert.strictEqual(minPq.size, 1);
+  });
+
+  it('adds smallest value at front in min priority queue', () => {
+    minPq.add(5);
+    assert.strictEqual(minPq.size, 2);
+    assert.strictEqual(minPq.peek(), 5);
+  });
+
+  it('adds element in the max priority queue', () => {
+    maxPq.add(10);
+    assert.strictEqual(maxPq.size, 1);
+  });
+
+  it('adds smallest value at front in min priority queue', () => {
+    maxPq.add(12);
+    assert.strictEqual(maxPq.size, 2);
+    assert.strictEqual(maxPq.peek(), 12);
+  });
+});
